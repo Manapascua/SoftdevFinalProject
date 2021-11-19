3c8baf02b4226978de17e1ba9f0c215254457847
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2021-11-20 00:23:15 +0800

    Created StudentController.java
    This file will contain the CRUD REST API mapping

---
+++ softdev-backend/src/main/java/com/example/softdevbackend/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.softdevbackend.controller;
+
+import java.util.List;
+
+import com.example.softdevbackend.model.Student;
+import com.example.softdevbackend.repository.StudentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
