dc0e95a83823f37e60c778a42a4edcd4de09b03a
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2021-11-20 00:39:38 +0800

    Update ResourceNotFoundException.java
    added RuntimeException class extension

--- softdev-backend/src/main/java/com/example/softdevbackend/exception/ResourceNotFoundException.java
+++ softdev-backend/src/main/java/com/example/softdevbackend/exception/ResourceNotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
-        super();
+        super(message);
     }
 }
