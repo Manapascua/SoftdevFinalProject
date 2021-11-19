0e890566f1d78a873c1407fe8f86abd4bfa86ef8
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2021-11-19 23:54:20 +0800

    Initial commit

---
+++ softdev-backend/src/main/java/com/example/softdevbackend/SoftdevBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.softdevbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftdevBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftdevBackendApplication.class, args);
+    }
+
+}
---
+++ softdev-backend/src/test/java/com/example/softdevbackend/SoftdevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softdevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftdevBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
