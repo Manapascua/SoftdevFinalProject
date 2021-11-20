73db99d17109ac5db32095d63844abb134d63054
pascojordan22 <pascojordan22@gmail.com>
2021-11-20 20:04:35 +0800

    Adding Header and Footer to React App

--- softdev-frontend/src/App.js
+++ softdev-frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 import ListStudentComponent from './components/ListStudentComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <HeaderComponent />
       <ListStudentComponent />
+      <FooterComponent />
     </div>
   );
 }
---
+++ softdev-frontend/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const FooterComponent = () => {
+    return (
+        <div>
+            <footer className ="footer">
+                <span className="text-muted"></span>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent
