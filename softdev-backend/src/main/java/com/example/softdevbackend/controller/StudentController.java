1afc13b83999cea91263c4fc23abaea19f4888ce
Emmanuel Pascua <Emmanuelpascua00@gmail.com>
2021-11-21 02:11:19 +1200

    Build GetStudent by Id REST API

--- softdev-backend/src/main/java/com/example/softdevbackend/controller/StudentController.java
+++ softdev-backend/src/main/java/com/example/softdevbackend/controller/StudentController.java
@@ -2,10 +2,12 @@ package com.example.softdevbackend.controller;
 
 import java.util.List;
 
+import com.example.softdevbackend.exception.ResourceNotFoundException;
 import com.example.softdevbackend.model.Student;
 import com.example.softdevbackend.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
@@ -24,5 +26,12 @@ public class StudentController {
     @PostMapping
     public Student createStudent(@RequestBody Student student){
     return studentRepository.save(student);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getStudentById(long id){
+        Student student = studentRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Student not exist with id"));
+        return ResponseEntity.ok(student);
     }
 }
